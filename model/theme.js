cadfffeaa9bcde9c428b92ee3062f0b8ac3d7af1
kellyztt <tiantong.zhou@wustl.edu>
2020-05-14 09:21:14 +0800

    async await

--- model/theme.js
+++ model/theme.js
@@ -3,8 +3,8 @@ import {Http} from "../utils/httpUtils";
 
 class Theme{
     //TODO: Why static?
-    static getHomeLocationA(callback){
-        Http.request({
+    static async getHomeLocationA(callback){
+        const data = await Http.request({
             url: '/theme/by/names',
             data: {
                 names: 't-1'
--- utils/httpUtils.js
+++ utils/httpUtils.js
@@ -2,21 +2,18 @@ import {config} from "../config/config";
 import {promisic} from "./util";
 
 class Http {
-    static request({url, data, callback, method='GET'}){
-        wx.request({
+    static async request({url, data, method='GET'}){
+        return await promisic(wx.request)({
             url: `${config.apiBaseUrl}${url}`,
             data,
             method,
             header: {
                 appkey: config.appkey
-            },
-            success: res => {
-                callback(res.data)
             }
         })
     }
 }
-promisic(wx.request)()
+
 export {
     Http
 }
